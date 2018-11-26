b2fcc75e44a28137ce42f00ad637619873ff83d2
Susan <Susan@Izzy>
2018-11-26 00:00:24 -0500

    Modificación 3...testing

--- xml-reader-demo/src/main/java/com/xml/reader/BigXmlTestBillIteratorApiApp.java
+++ xml-reader-demo/src/main/java/com/xml/reader/BigXmlTestBillIteratorApiApp.java
@@ -35,10 +35,6 @@ public class BigXmlTestBillIteratorApiApp {
 	private static final String CONTENT_ELEMENT = "BILLS";
 	private static final String DATA_ELEMENT = "BILL";
 	
-	
-	//Susan
-	
-	
 	public static final String DATE_FORMAT = "dd/MM/yyyy hh:mm:ss";
 	public static final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 
